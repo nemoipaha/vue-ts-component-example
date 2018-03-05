01f371e5e8de608909be49b94bba7eaa75a89fb0
pavlo <pavelnemoi@gmail.com>
2018-03-05 12:00:40 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div>
+      <hello-component :msg="msg" :initialEnthusiasm="1" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Hello from './components/Hello.vue'
+
+@Component({
+    components: {
+        'hello-component': Hello
+    }
+})
+export default class App extends Vue {
+  msg: string = 'Welcome to Your Vue.js App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="greeting">{{ msg }} {{ exclamationMarks }}</div>
+        <button @click="decrement">-</button>
+        <button @click="increment">+</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import {Component, Prop} from "vue-property-decorator";
+import Component from 'vue-class-component';
+
+@Component({
+    name: 'HelloComponent',
+    props: {
+        msg: String,
+        initialEnthusiasm: Number
+    },
+})
+export default class Hello extends Vue {
+    msg: string
+
+    initialEnthusiasm: number
+
+    enthusiasm = this.initialEnthusiasm
+
+    increment(): void {
+        this.enthusiasm++;
+    }
+
+    decrement(): void {
+        if (this.enthusiasm > 1) {
+            this.enthusiasm--;
+        }
+    }
+
+    get exclamationMarks(): string {
+        return Array(this.enthusiasm + 1).join('!');
+    }
+}
+</script>
+
+<style>
+    .greeting {
+        font-size: 20px;
+    }
+</style>
